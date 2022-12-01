153fb92c964fef421ac26128918341aed4dfccbf
Joyce Chen <joyceideas@outlook.com>
2022-12-01 00:46:56 -0500

    edit components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Footer() {
+  return (
+    <div>
+      <h3>Footer</h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from "./components/Nav";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <div>
+      Joyce
+    </div>
+  )
+}
+
+export default Header
