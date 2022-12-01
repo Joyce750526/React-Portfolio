3bae0148229d657ec829ce1dd2769db03a8f5dab
Joyce Chen <joyceideas@outlook.com>
2022-12-01 00:44:58 -0500

    edited about page

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Portfolio() {
+  return (
+    <div>
+      <h3>WILL COME SOON </h3>
+    </div>
+  )
+}
+
+export default Portfolio
---
+++ src/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Resume() {
+  return (
+    <div>
+      <h3>ABC</h3>
+    </div>
+  )
+}
+
+export default Resume
