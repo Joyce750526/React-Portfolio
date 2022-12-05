bec5dc4a87c6dd8de1f8e71f352be00c0af38f66
Joyce Chen <joyceideas@outlook.com>
2022-12-05 03:09:11 -0500

    editing resume format. it is working but still in working progress

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,24 +1,219 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import Resume from "./../../files/Joyce-Resume.pdf";
 
 export default function Resume() {
   return (
-  <Card>
-  <Card.Header>Quote</Card.Header>
-  <Card.Body>
-    <blockquote className="blockquote mb-0">
-      <p>
-        {' '}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-        posuere erat a ante.{' '}
-      </p>
-      <footer className="blockquote-footer">
-        Someone famous in <cite title="Source Title">Source Title</cite>
-      </footer>
-    </blockquote>
-  </Card.Body>
-</Card>
+    <div className="container">
+      <div className="container text-left">
+        {/* Education Section */}
+        <div className="col-lg-12">
+          <br />
+          <h4 style={{ fontWeight: "normal" }}>Education</h4>
+          <ul>
+            <li>B.F.A. Interior Architecture & Design | May 2018</li>
+            <li>Academy of Art University, San Francisco, CA</li>
+          </ul>
+          <ul>
+            <li>Certificate Full-Stack Coding BootCamp | Dec 2022</li>
+            <li>University of New Hampshire, New Hampshire, MA</li>
+          </ul>
+        </div>
+
+        {/* Career Path Section */}
+        <div className="col-lg-12">
+          <h4 style={{ fontWeight: "normal" }}>Career Path</h4>
+          <h6 style={{ fontWeight: "bold" }}>
+            University of New Hampshire • Full StackCoding BootCamp | 24 Weeks
+          </h6>
+          <ul>
+            <li>
+              Learning browser based technologics, API design, Databases, and
+              server side development
+            </li>
+            <li>
+              Understand the basics of troubleshooting and enhancing legacy code
+            </li>
+            <li>
+              Learning how to build a front end website with implementing
+              complex logical conditions
+            </li>
+            <li>Learning towards a goal to create a complex team project</li>
+          </ul>
+          <h6 style={{ fontWeight: "bold" }}>
+            The Tech Foundry | IT Live Course | 16 Weeks
+          </h6>
+          <ul>
+            <li>
+              Basic IT knowledge training with hardware troubleshooting to help
+              solve the issue
+            </li>
+            <li>
+              Learned system administration: Windows Server and Active Directory
+            </li>
+            <li>
+              Learned operating systems: Linux (OS Terminal) and Windows
+              (PowerShell)
+            </li>
+            <li>Learned Network services: DHCP and DNS</li>
+            <li>
+              Learned imaging and deployment to install software on computers
+            </li>
+          </ul>
+          <h6 style={{ fontWeight: "bold" }}>
+            ELVTR | UX Live Course | 12 Weeks
+          </h6>
+          <ul>
+            <li>UX design skills in a B2B software environment</li>
+            <li>
+              UX research- Ideate the solution, wireframing & prototyping,
+              usability testing
+            </li>
+            <li>
+              Redesigned Airbnb adding feature and share function on APP to make
+              it more usable
+            </li>
+          </ul>
+          <h6 style={{ fontWeight: "bold" }}>
+            {" "}
+            Holyoke Community College • The Accelerated English Course | Current
+          </h6>
+          <ul>
+            <li>
+              Enhancing advanced level in grammar, listening, and writing for
+              professional skills
+            </li>
+            <li>
+              Enriching English skills for higher education preparation and
+              career change
+            </li>
+            <li>
+              Learning professional interview skills and receiving career
+              coaching for career prep
+            </li>
+          </ul>
+        </div>
+
+        {/* Work Experience Section */}
+        <div className="col-lg-12">
+          <h4 style={{ fontWeight: "normal" }}>Work Experience</h4>
+          <h6 style={{ fontWeight: "bold" }}>IT Internship • Tech Foundry</h6>
+          <ul>
+            <li>Reviewed 30 weeks Google IT courses for Tech Foundry</li>
+            <li>
+              Created documentations and summarized a 30 weeks Google IT Support
+              course
+            </li>
+            <li>
+              Explored Amazon Web Service (S3) for a hosting static website
+            </li>
+            <li>
+              Worked on WordPress to prepare creating a professional website
+            </li>
+          </ul>
+          <h6 style={{ fontWeight: "bold" }}>
+            Intermediate Designer • MA Dimensions Inc
+          </h6>
+          <ul>
+            <li>
+              Familiar with city submittal process for residential and
+              commercial permit
+            </li>
+            <li>
+              Onsite measuring for projects, creating floor plan and
+              construction document set
+            </li>
+            <li>
+              Solutionional design for projects with knowledge of regulations
+            </li>
+            <li>
+              Onsite/online meetings with clients to go over the design process/
+              options
+            </li>
+            <li>
+              Collaboratively with MEP engineers on the design goal to meet
+              deadlines
+            </li>
+          </ul>
+          <h6 style={{ fontWeight: "bold" }}>
+            Interior Design Consultant • Gooder Supply Home Design, Inc
+          </h6>
+          <ul>
+            <li>
+              Created 2D floor plans for cabinetry using in house software 20/20
+              for clients
+            </li>
+            <li>
+              Helping clients to pick needed products to bring company values
+            </li>
+            <li>
+              Work closely with clients to their needs and offer solutions
+              through space design
+            </li>
+          </ul>
+          <h6 style={{ fontWeight: "bold" }}>
+            Design Consultant Experience • Harley Ellis Devereaux (HED)
+          </h6>
+          <ul>
+            <li>
+              Combined a portfolio of six projects on time with high quality
+              aligned with requirements from the project architects
+            </li>
+            <li>
+              Structured bookmarks and created hyperlinks for navigation to all
+              single drawing and to schematics into PDFs in Bluebeam Revu to
+              meet DSA requirements
+            </li>
+          </ul>
+
+          {/* Technical Skills Section */}
+          <h4 style={{ fontWeight: "normal" }}>Technical Skills</h4>
+          <h6 style={{ fontWeight: "bold" }}>Front-End Technologies</h6>
+          <ol>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>BootStrap</li>
+            <li>React</li>
+          </ol>
+          <h6 style={{ fontWeight: "bold" }}>Back-End Technologies</h6>
+          <ol>
+            <li>MySQL | Sequelize</li>
+            <li>Mongo | Mongoose</li>
+            <li>Express.js</li>
+            <li>Node.js</li>
+            <li>APIs</li>
+          </ol>
+        </div>
+      </div>
+    </div>
   );
 }
+
+/* <ul class="list-group list-group-flush">
+<li class="list-group-item">
+  Front-End: HTML, CSS, JavaScript, JQuery, Bootstrap, React
+</li>
+<li class="list-group-item">
+  Back-End: MySQL | Sequelize, Mongo | Mongoose, Express.js,
+  Node.js, APIs
+</li>
+</ul> */
+
+/* <Card>
+<Card.Header>Quote</Card.Header>
+<Card.Body>
+  <blockquote className="blockquote mb-0">
+    <p>
+      {' '}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+      posuere erat a ante.{' '}
+    </p>
+    <footer className="blockquote-footer">
+      Someone famous in <cite title="Source Title">Source Title</cite>
+    </footer>
+  </blockquote>
+</Card.Body>
+</Card> */
