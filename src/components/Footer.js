f58be358eeb925f0a18b992bf1835d4d89eca44c
Joyce Chen <joyceideas@outlook.com>
2022-12-05 02:47:34 -0500

    updated footer page for now. still in working progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
-// import React from 'react';
-// import './../../styles/Footer.css';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Footer() {
   return (
-    <div>
-      <p align="center">Footer</p>
-    </div>
-  )
+  <p align="center">© 2022 Copyright • Chao-Ying Chen</p>
+  
+  );
 }
 
 
- 
+// Saved Codes for later- this React website is still in working progress
+
+// https://github.com/Joyce750526
+// https://www.linkedin.com/in/joyce-chen-0526/
+// <p>UNH Coding BootCamp</p>
+// <p>© 2022 Copyright • Chao-Ying Chen</p>
+
+/* <footer className="foot-icons">
+  <a href="https://github.com/Joyce750526" target="_blank" rel="noreferrer"></a>
+  <img
+    className="footer-icon"
+    src="https://drive.google.com/file/d/12j7GoDGQF7sYgw0XEQE66JalMZNfRwOf/view?usp=sharing"
+    alt="GitGub"
+  ></img>
+</footer>; */
