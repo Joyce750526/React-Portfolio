623a0bcb4a830566052fb8aef2d34cf074357e07
Joyce Chen <joyceideas@outlook.com>
2023-08-03 21:27:57 -0400

    fix rel link in Footer.js to prevent the risks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,51 @@
 import React from "react";
 import { Github } from "react-bootstrap-icons";
 import { Linkedin } from "react-bootstrap-icons";
-import {FiletypePdf } from "react-bootstrap-icons";
-import { EnvelopeAtFill} from "react-bootstrap-icons";
+import { FiletypePdf } from "react-bootstrap-icons";
+import { EnvelopeAtFill } from "react-bootstrap-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Footer() {
   return (
     <div align="center">
-
+      eslint-disable-next-line
       {/* Hyberlinked GitHub Icon */}
-      <a href="https://github.com/Joyce750526" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/Joyce750526"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Github color="black" size={20} />
       </a>
-      
       {/* Hyberlinked LinkedIn Icon */}
-      <a href="https://www.linkedin.com/in/joyce-chen-0526/" target="_blank" rel="noreferrer">
+      <a
+        href="https://www.linkedin.com/in/joyce-chen-0526/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Linkedin color="blue" size={20} />
       </a>
-
       {/* Hyberlinked Email Icon */}
-      <a href="mailto:joyceideas@outlook.com"> 
-      <EnvelopeAtFill color="black" size={20} target="_blank" rel="noreferrer"/>
-      </a>  
-
-      {/* Hyberlinked Viwe Resume Need to Fix this! */}
-      <a href="src/files/resume.pdf" > 
-      <FiletypePdf color="black" size={20} />
+      <a href="mailto:joyceideas@outlook.com">
+        <EnvelopeAtFill
+          color="black"
+          size={20}
+          target="_blank"
+          rel="noreferrer"
+        />
       </a>
-
-
-      <br/>
-
+      {/* Hyberlinked Viwe Resume Need to Fix this! */}
+      <a href="src/files/resume.pdf">
+        <FiletypePdf color="black" size={20} />
+      </a>
+      <br />
       {/* Copyright Text */}
-      <p align="center" style={{ fontSize: "small" }}>© 2022 Copyright • Chao-Ying Chen</p>
-    
+      <p align="center" style={{ fontSize: "small" }}>
+        © 2022 Copyright • Chao-Ying Chen
+      </p>
     </div>
   );
 }
-
 
 // Saved Codes for later- this React website is still in working progress
 
