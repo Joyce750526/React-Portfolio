e73612585bbf1578811c80d65a8820ed5da20e6e
Joyce Chen <joyceideas@outlook.com>
2023-08-16 04:24:27 -0400

    working on typewriter effecto make homepage more interesting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/Home.css';
+import React, { useState, useEffect } from "react";
+import Typewriter from "typewriter-effect";
+import "../styles/Home.css"; // Make sure to adjust the path to your CSS file
 
 export default function Home() {
-  const [typedIndex, setTypedIndex] = useState(0);
-  const roles = ['Full-Stack Web Developer', 'Interior Designer'];
+  const roles = [
+    "HTML | CSS | JavaScript | React",
+    "a Frontend Software Developer!",
+    "an Interior Designer",
+    "Aspiring Developer!",
+    "Love Tech!"
+  ];
+
+  const [typedText, setTypedText] = useState(roles[0]); // Initialize with first role
+  const [typedIndex, setTypedIndex] = useState(1); // Start from the second role
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTypedIndex((prevIndex) => (prevIndex + 1) % roles.length);
-    }, 2000); // Change role every 2 seconds
+      setTypedText(roles[typedIndex]);
+    }, 2000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [typedIndex]);
 
   return (
     <div className="home-container">
       <div className="jumbotron">
         <div className="hero-container" data-aos="fade-in">
-          <h1>Chao Ying Chen</h1>
+          <h1>Hello !!</h1>
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 50,
+              strings: [roles[0]],
+            }}
+          />
           <div>
             <p>
-              I'm an {roles[typedIndex]}
+              I'm{" "}
+              <span className="typed">
+                <u>{typedText}</u>
+              </span>
               <span
                 className="typed-cursor typed-cursor--blink"
                 aria-hidden="true"
-              >
-                |
-              </span>
+              ></span>
             </p>
           </div>
         </div>
